fac70dffe91e80932cf5c758d5a6cf8d1b1dc5bb
cristinescheiquilay <scheiyaliuq@gmail.com>
2025-03-16 16:59:17 +0800

    Submitting Python Week 05 - Laboratory # 21

--- factorial_calculator_functions.py
+++ factorial_calculator_functions.py
@@ -6,18 +6,15 @@ def get_non_negative_integer() -> int:
                 return num
             print("Error: Please enter a non-negative integer.")  # No need for else
         except ValueError:
-            print("Error: Invalid input. Please enter a valid integer.")
-
+            print("Error: Invalid input. Please enter a valid integer."
 def calculate_factorial(n: int) -> int:
     factorial = 1
     for i in range(1, n + 1):
         factorial *= i
     return factorial
-
 def main():
     number = get_non_negative_integer()
     result = calculate_factorial(number)
     print(f"The factorial of {number} is: {result}")
-
 if name == "main":
     main()
